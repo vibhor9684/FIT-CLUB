0c5cf76224fae9550e7ce524036278e1ff876db8
vibhor9684 <vibhor9684@gmail.com>
2024-07-25 13:15:19 +0530

    rigth section started

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import "./Hero.css"
 import Header from '../Header/Header'
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
 
 
 const Hero = () => {
@@ -9,7 +13,7 @@ const Hero = () => {
         <div className="left-h"><Header/>
         {/* the best ad  */}
         <div className="the-best-ad">
-            <div></div>
+            <div></div>  
             <span>the best fitness club in the town</span>
         </div>
 
@@ -25,9 +29,37 @@ const Hero = () => {
 
         </div>
         {/* figures */}
+        <div className="figures">
+          <div>
+          <span>+140</span>
+          <span>expert coachs</span>
+          </div>
+          <div>
+          <span>+978</span>
+          <span>member joint</span>
+          </div>
+          <div>
+          <span>+50</span>
+          <span>fitness program</span>
+          </div>
         </div>
-        
-        <div className="right-h">right side</div>
+        {/*hero button */}
+        <div className="hero-buttons">
+          <buttons className="btn">Get Started</buttons>
+          <buttons className="btn">Learn More</buttons>
+        </div>
+
+        </div>
+        <div className="right-h">
+          <buttons className="btn">Join Now</buttons>
+
+          <div className="heart-rate">
+            <img src={Heart} alt="" />
+            <span>Heart Rate</span>
+            <span>116 bpm</span>
+          </div>
+
+        </div>
         
     </div>
   )
