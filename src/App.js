fe5b2a00a3eeabc715f04825bfd73718e7e9f005
vibhor9684 <vibhor9684@gmail.com>
2024-07-27 16:40:04 +0530

    grid section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs.jsx';
+import Reasons from './components/Reasons/Reasons.jsx';
 function App() {
   return (
     <div className="App">
           <Hero/>
           <Programs/>
+          <Reasons/>
     </div>
   );
 }
